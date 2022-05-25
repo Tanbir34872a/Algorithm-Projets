046938f97ff25bca1faf582b3cdf3d3a82ccc68c
Tanbir34872a <yousuf.tanbir@gmail.com>
2022-05-25 22:49:35 +0600

    Create Prime Number.cpp

---
+++ HW 1/Prime Number.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void prime_find_linear(int val){
+    for(int a = 2; a < val; a++){
+        if(val % a == 0){cout<<"It's not a prime number";return;}
+    }
+    cout<<"It's a prime number";
+}
+
+void prime_find_rooted(int val){
+    for(int a = 2; a < sqrt(val); a++){
+        if(val % a == 0){cout<<"It's not a prime number";return;}
+    }
+    cout<<"It's a prime number";
+}
+
+int main(){
+    //Comment either one of them
+    //prime_find_linear(1111122851);
+    prime_find_rooted(1111122851);
+    return 0;
+}
